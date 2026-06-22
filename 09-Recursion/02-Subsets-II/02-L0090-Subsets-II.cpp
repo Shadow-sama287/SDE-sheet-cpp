bce67b96d7bbcbc9944c768619d6be1c2e39bbbd
Karan Chavan <karanchavan287@gmail.com>
2026-06-23 00:41:25 +0530

    feat: Subset II | Medium
    only unique subsets.

---
+++ 09-Recursion/02-Subsets-II/02-L0090-Subsets-II.cpp
@@ -0,0 +1,34 @@
+class Solution
+{
+public:
+    void getAllSubsets(vector<int> &nums, vector<int> &ans, int i, vector<vector<int>> &allSubsets)
+    {
+        if (i == nums.size())
+        {
+            allSubsets.push_back({ans});
+            return;
+        }
+        // inclusion
+        ans.push_back(nums[i]);
+        getAllSubsets(nums, ans, i + 1, allSubsets);
+
+        ans.pop_back(); // backtracking
+        int idx = i + 1;
+        while (idx <= nums.size() - 1 && nums[idx] == nums[idx - 1])
+            idx++;
+        // exclusion
+        getAllSubsets(nums, ans, idx, allSubsets);
+    }
+
+    vector<vector<int>> subsetsWithDup(vector<int> &nums)
+    {
+        vector<vector<int>> allSubsets;
+        vector<int> ans;
+
+        sort(nums.begin(), nums.end());
+
+        getAllSubsets(nums, ans, 0, allSubsets);
+
+        return allSubsets;
+    }
+};
