07d4aa758ce808a92721561aef80257c29ebb910
Karan Chavan <karanchavan287@gmail.com>
2026-06-26 01:04:14 +0530

    feat: Implement Queue using Stack | Easy

---
+++ 13-Stack-n-Queue/04-Implement-Queue-Using-Stack/04-L232-Implement-Queue-Using-Stack.cpp
@@ -0,0 +1,63 @@
+class MyQueue
+{
+public:
+    stack<int> in;
+    stack<int> out;
+
+    MyQueue() {}
+
+    void push(int x) { in.push(x); }
+
+    int pop()
+    {
+        if (out.empty())
+        {
+            while (!in.empty())
+            {
+                out.push(in.top());
+                in.pop();
+            }
+        }
+        // If queue is still empty, return -1 (or throw an error if
+        // preferred)
+        if (out.empty())
+        {
+            return -1;
+        }
+
+        int x = out.top();
+        out.pop();
+        return x;
+    }
+
+    int peek()
+    {
+        if (out.empty())
+        {
+            while (!in.empty())
+            {
+                out.push(in.top());
+                in.pop();
+            }
+        }
+        // If queue is still empty, return -1 (or throw an error if
+        // preferred)
+        if (out.empty())
+        {
+            return -1;
+        }
+
+        return out.top();
+    }
+
+    bool empty() { return in.empty() && out.empty(); }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
